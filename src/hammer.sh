51188bb546b440babf6def2db2915071032425d2
Mario Carneiro <di.gama@gmail.com>
2023-02-21 10:00:14 -0500

    tweak names

--- src/hammer.sh
+++ src/hammer.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 n0=$1
-bin/mmportclassexec tofof tmp/$n0.mm0 tmp/$n0.p .
-bin/mmportclassexec tolisp tmp/$n0.mm0 tmp/$n0.p.lisp .
+bin/translate tofof tmp/$n0.mm0 tmp/$n0.p .
+bin/translate tolisp tmp/$n0.mm0 tmp/$n0.p.lisp .
 perl -i -pe 's/\baxiom\b/conjecture/' tmp/$n0.p tmp/$n0.p.lisp
 n="$n0.p"
 grep conjecture tmp/$n | bin/features  -const -trm0 -sub0 -pat 2 -gen /dev/stdin \
   | cut -f2 -d\:  > tmp/$n.fea
 time bin/predict \
-  build/mmsetclassv1.ax.fea build/mmsetclassv1.ax.deps build/mmsetclassv1.ax.seq \
+  build/setmm.ax.fea build/setmm.ax.deps build/setmm.ax.seq \
   -p knn -n 960 < tmp/$n.fea > tmp/$n.knn960
-src/mkprobs1.pl build/mmsetclassv1.ax tmp/$n tmp/$n.knn960 960:480:240:120:80:40
+src/mkprobs1.pl build/setmm.ax tmp/$n tmp/$n.knn960 960:480:240:120:80:40
