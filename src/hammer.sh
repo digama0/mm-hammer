f0d72161932a0a2726c9a9eeb2e6b85d55ca1f93
Mario Carneiro <di.gama@gmail.com>
2023-02-21 09:59:21 -0500

    fix

--- src/hammer.sh
+++ src/hammer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 n0=$1
-build/mmportclassexec tofof tmp/$n0.mm0 tmp/$n0.p .
-build/mmportclassexec tolisp tmp/$n0.mm0 tmp/$n0.p.lisp .
+bin/mmportclassexec tofof tmp/$n0.mm0 tmp/$n0.p .
+bin/mmportclassexec tolisp tmp/$n0.mm0 tmp/$n0.p.lisp .
 perl -i -pe 's/\baxiom\b/conjecture/' tmp/$n0.p tmp/$n0.p.lisp
 n="$n0.p"
 grep conjecture tmp/$n | bin/features  -const -trm0 -sub0 -pat 2 -gen /dev/stdin \
